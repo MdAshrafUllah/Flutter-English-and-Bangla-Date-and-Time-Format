f4f172a6b1085ebd211424f6650c5638d0414e2b
Md Ashraf Ullah <96839511+MdAshrafUllah@users.noreply.github.com>
2024-01-28 16:13:35 +0600

    Add files via upload

---
+++ Date format 9/date_format_9.dart
@@ -0,0 +1,23 @@
+String formatCurrentDate() {
+  DateTime now = DateTime.now();
+  return '${now.day.toString().padLeft(2, '0')} ${monthToString(now.month)} ${now.year.toString().substring(2)}';
+}
+
+String monthToString(int month) {
+  const List<String> months = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December'
+  ];
+
+  return months[month - 1];
+}
